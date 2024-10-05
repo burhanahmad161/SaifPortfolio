843e949eafa68d0e81ec1765f8a0ade9502d0741
Burhan Ahmad <burhanahmad1616j@gmail.com>
2024-10-05 16:22:18 +0500

    gitignore

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ import { MdMiscellaneousServices } from "react-icons/md";
 
 export default function Home() {
   return (
-     <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 overflow-clip">
+     <main className=" relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
    
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={[
